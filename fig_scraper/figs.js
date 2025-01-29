ff50915db314951305dee84c6d8fe028b0b807d5
brendanml <bml1940@gmail.com>
2025-01-29 08:44:41 -0800

    web scraper working... pre setting backend endpoints for microservices data

--- fig_scraper/figs.js
+++ fig_scraper/figs.js
@@ -1,24 +1,36 @@
-
-
-
-
+require('dotenv').config();
 const OAuth = require('oauth');
-// const { Parser } = require('json2csv');
-// const fs = require('fs');
 
-var oauth = new OAuth.OAuth('', '', CONS_KEY, CONS_SEC, '1.0', null, 'HMAC-SHA1');
+// Access environment variables
+const CONS_KEY = process.env.VITE_CON_KEY;
+const CONS_SEC = process.env.VITE_CON_SEC;
+const ACCESS_TOKEN = process.env.VITE_ACC_TOK;
+const ACCESS_SECRET = process.env.VITE_ACC_SEC;
+
+// Initialize OAuth
+let oauth = new OAuth.OAuth('', '', CONS_KEY, CONS_SEC, '1.0', null, 'HMAC-SHA1');
+
+// Define fields (optional, as per your use case)
 const fields = [{
     label: 'Item Name',
     value: 'item.name'
 }, {
     label: 'Colour',
     value: 'color_name'
-}]
+}];
 
-oauth.get('https://api.bricklink.com/api/store/v1/inventories', ACCESS_TOKEN, ACCESS_SECRET, function(error, data, res) {
-    if (error) console.error(error);
-
-    var obj = JSON.parse(data);
-
-    console.log(obj)
-});
+// Use the API
+oauth.get(
+    'https://api.bricklink.com/api/store/v1/inventories',
+    ACCESS_TOKEN,
+    ACCESS_SECRET,
+    function (error, data, res) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        // Parse and log the response data
+        const obj = JSON.parse(data);
+        console.log(obj);
+    }
+);
